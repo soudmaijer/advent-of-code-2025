23300bb7d0002bfa5a67f6bf9bdffab6bd8d36d6
Stephan Oudmaijer <soudmaijer@gmail.com>
2025-12-01 14:56:58 +0100

    overengineered version

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,49 +1,67 @@
+import Direction.BACK
+import Direction.FORWARD
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.io.path.Path
 import kotlin.io.path.forEachLine
 
-fun part1() {
+enum class Direction(private val direction: Char) {
+    BACK('L'),
+    FORWARD('R');
+
+    companion object {
+        fun from(direction: Char): Direction {
+            return when (direction) {
+                BACK.direction -> BACK
+                FORWARD.direction -> FORWARD
+                else -> throw IllegalArgumentException()
+            }
+        }
+    }
+}
+
+/**
+ * Process the input file and invoke the counter function for each line.
+ */
+fun inputIterator(count: (CircularList<Int>, Int, Direction, AtomicInteger) -> Unit): AtomicInteger {
     val list = CircularList((0..99).toList()).apply { moveToPosition(50) }
-    var password = 0
+    val password = AtomicInteger(0)
 
     Path("src/main/resources/day-01-input.txt").forEachLine { line ->
         val trimmed = line.trim()
         val steps = trimmed.substring(1).toInt()
-        when(trimmed.first()) {
-            'L' -> if(list.stepBack(steps) == 0) password++ // stepBack() returns current position
-            'R' -> if(list.stepForward(steps) == 0) password++ // stepForward() returns current position
-            else -> throw IllegalArgumentException("Invalid step $trimmed")
-        }
+        count(list, steps, Direction.from(trimmed.first()), password)
     }
+    return password
+}
 
+/**
+ * Count number of times we hit position 0 after the specified number of steps.
+ */
+fun countZeroAfterSteps(list: CircularList<Int>, steps: Int, direction: Direction, counter: AtomicInteger) {
+    if (BACK == direction && list.stepBack(steps) == 0) counter.incrementAndGet()
+    if (FORWARD == direction && list.stepForward(steps) == 0) counter.incrementAndGet()
+}
+
+/**
+ * Count number of times we hit position 0 on each iteration of the number of steps.
+ */
+fun countZeroAfterEachStep(list: CircularList<Int>, steps: Int, direction: Direction, counter: AtomicInteger) {
+    repeat(steps) {
+        countZeroAfterSteps(list, steps = 1, direction = direction, counter = counter)
+    }
+}
+
+fun day1a() {
+    val password = inputIterator(::countZeroAfterSteps)
     println("Part 1 - Password: $password")
 }
 
-fun part2() {
-    val list = CircularList((0..99).toList()).apply { moveToPosition(50) }
-    var password = 0
-
-    Path("src/main/resources/day-01-input.txt").forEachLine { line ->
-        val trimmed = line.trim()
-        var steps = trimmed.substring(1).toInt()
-        when(trimmed.first()) {
-            'L' -> {
-                while (steps-- > 0) {
-                    if(list.stepBack() == 0) password++ // stepBack() returns current position
-                }
-            }
-            'R' -> {
-                while (steps-- > 0) {
-                    if(list.stepForward() == 0) password++ // stepForward() returns current position
-                }
-            }
-            else -> throw IllegalArgumentException("Invalid step $trimmed")
-        }
-    }
-
+fun day1b() {
+    val password = inputIterator(::countZeroAfterEachStep)
     println("Part 2 - Password: $password")
 }
 
 fun main() {
-    part1()
-    part2()
+    day1a()
+    day1b()
 }
