c11aee3b0e7d3694b09248a4b8bbf37d67107bc9
Stephan Oudmaijer <soudmaijer@gmail.com>
2025-12-01 12:58:51 +0100

    Part 1 and 2.

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,48 @@
+import kotlin.io.path.Path
+import kotlin.io.path.forEachLine
+
+fun part1() {
+    val list = CircularList((0..99).toList()).apply { moveToPosition(50) }
+    var password = 0
+
+    Path("src/main/resources/day-01-input.txt").forEachLine { line ->
+        val trimmed = line.trim()
+        val steps = trimmed.substring(1).toInt()
+        when(trimmed.first()) {
+            'L' -> list.stepBack(steps)
+            else -> list.stepForward(steps)
+        }
+        if(list.currentPosition() == 0) password++
+    }
+
+    println("Part 1 - Password: $password")
+}
+
+fun part2() {
+    val list = CircularList((0..99).toList()).apply { moveToPosition(50) }
+    var password = 0
+
+    Path("src/main/resources/day-01-input.txt").forEachLine { line ->
+        val trimmed = line.trim()
+        var steps = trimmed.substring(1).toInt()
+        when(trimmed.first()) {
+            'L' -> {
+                while (steps-- > 0) {
+                    if(list.stepBack() == 0) password++ // stepBack() returns current position
+                }
+            }
+            else -> {
+                while (steps-- > 0) {
+                    if(list.stepForward() == 0) password++ // stepForward() returns current position
+                }
+            }
+        }
+    }
+
+    println("Part 2 - Password: $password")
+}
+
+fun main() {
+    part1()
+    part2()
+}
